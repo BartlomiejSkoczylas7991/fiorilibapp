6bfbd906c3ea6e057fa7e093b3df04295ac6d181
barlomiejSkoczylas <barlomiej.skoczylas@fioneer.com>
2024-03-05 07:22:48 +0100

    save

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,22 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("fiorilibappname.controller.App", {
-        onInit: function() {
-        }
-      });
-    }
-  );
-  
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], function(BaseController) {
+  "use strict";
+
+  return BaseController.extend("fiorilibappname.controller.App", {
+      onInit: function() {
+      },
+
+      onAfterRendering: function() {
+          var oElement = this.getView().$();
+          while (oElement && oElement.hasClass) {
+              if (oElement.hasClass("sapUShellApplicationContainerLimitedWidth")) {
+                  oElement.removeClass("sapUShellApplicationContainerLimitedWidth");
+                  break;
+              }
+              oElement = oElement.parent();
+          }
+      }
+
+  });
+});
