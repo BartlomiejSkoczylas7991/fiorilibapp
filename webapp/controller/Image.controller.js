07488b1af3d751431ebfc9ba95227b1b0c0161fa
barlomiejSkoczylas <barlomiej.skoczylas@fioneer.com>
2024-03-21 09:42:20 +0100

    Last working version

--- webapp/controller/Image.controller.js
+++ webapp/controller/Image.controller.js
@@ -1,55 +1,91 @@
 sap.ui.define([
     "fiorilibappname/controller/Object.controller",
-    "sap/m/library",
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/Item",
-	"sap/ui/model/json/JSONModel",
-	"sap/m/upload/Uploader",
-	"sap/m/StandardListItem",
+    //"sap/m/library",
+	//"sap/ui/core/mvc/Controller",
+	//"sap/ui/core/Item",
+	//"sap/ui/model/json/JSONModel",
+	//"sap/m/upload/Uploader",
+	//"sap/m/StandardListItem",
 	"sap/m/MessageToast"
-], function (ObjectController, MobileLibrary, Controller, Item, JSONModel, Uploader, ListItem, MessageToast) {
+], function (ObjectController, MessageToast) {
     "use strict";
 
-    var ListMode = MobileLibrary.ListMode;
-    var CustomUploader = Uploader.extend("sap.m.sample.UploadSetCustomUploader.CustomUploader", {
-        metadata: {}
-    });
-
-    CustomUploader.prototype.uploadItem = functiom (oItem, aHeaders) {
-        var sNewUplaodUrl = "../../../../upload";
-        aHeaders.push(new Item)
-    };
-
-    CustomUploader.prototype.downloadItem = functiom (oItem, aHeaders, bAskForLocation){
-        var sNewDownloadUrl = oItem.getUrl();
-        aHeaders.push(new Item({key: "SomeGetKey", text: "SomeGetText"}));
-        this.setDownloadUrl(sNewDownloadUrl);
-
-        Uploader.prototype.downloadItem.call(this, oItem, aHeaders, bAskForLocation);
-    };
+    //var ListMode = MobileLibrary.ListMode;
+    //var CustomUploader = Uploader.extend("sap.m.sample.UploadSetCustomUploader.CustomUploader", {
+    //    metadata: {}
+    //});
+//
+    //CustomUploader.prototype.uploadItem = function (oItem, aHeaders) {
+	//	var sNewUploadUrl = "../../../../upload"; // This value may be result of a backend request eg.
+	//	aHeaders.push(new Item({key: "SomePostKey", text: "SomePostText"}));
+	//	this.setUploadUrl(sNewUploadUrl);
+//
+	//	Uploader.prototype.uploadItem.call(this, oItem, aHeaders);
+	//};
+//
+	//CustomUploader.prototype.downloadItem = function (oItem, aHeaders, bAskForLocation) {
+	//	var sNewDownloadUrl = oItem.getUrl(); // This value may be result of a backend request eg.
+	//	aHeaders.push(new Item({key: "SomeGetKey", text: "SomeGetText"}));
+	//	this.setDownloadUrl(sNewDownloadUrl);
+//
+	//	Uploader.prototype.downloadItem.call(this, oItem, aHeaders, bAskForLocation);
+	//};
 
     return ObjectController.extend("fiorilibappname.controller.Image", {
         onInit: function () {
             ObjectController.prototype.onInit.apply(this, arguments);
             
 
-            // by uploader
-            var sPath = sap.ui.require.toUrl("sap/m/sample/UploadSetCustomUploader/items.json"),
-                oUploadSet = this.byId("UploadSet"),
-                oCustomUploader = new CustomUploader();
-
-            this.getView().setModel(new JSONModel(sPath));
-
-            oUploadSet.setUploader(oCustomUploader);
-            oUploadSet.regusterUploadedEvents(oCustomUploader);
-
-            oCustomUploader.attachUploadStarted(this.onUploadStarted.bind(this));
-            oCustomUploader.attachUploadProgressed(this.onUploadProgressed.bind(this));
-            oCustomUploader.attachUploadCompleted(this.onUploadCompleted.bind(this));
-            oCustomUploader.attachUploadAborted(this.onUploadAborted.bind(this));
-
-            oUploadSet.getList
+            //// by uploader
+            //var sPath = sap.ui.require.toUrl("sap/m/sample/UploadSetCustomUploader/items.json"),
+            //    oUploadSet = this.byId("UploadSet"),
+            //    oCustomUploader = new CustomUploader();
+//
+            //this.getView().setModel(new JSONModel(sPath));
+//
+            //oUploadSet.setUploader(oCustomUploader);
+            //oUploadSet.regusterUploadedEvents(oCustomUploader);
+//
+            //oCustomUploader.attachUploadStarted(this.onUploadStarted.bind(this));
+            //oCustomUploader.attachUploadProgressed(this.onUploadProgressed.bind(this));
+            //oCustomUploader.attachUploadCompleted(this.onUploadCompleted.bind(this));
+            //oCustomUploader.attachUploadAborted(this.onUploadAborted.bind(this));
+//
+            //oUploadSet.getList().setMode(ListMode.MultiSelect);
         },
+        //onUploadStarted: function (oEvent) {
+		//	var oList = this.byId("progressList"),
+		//		oItem = oEvent.getParameter("item");
+		//	oList.insertItem(new ListItem({
+		//		title: "Upload started: " + oItem.getFileName()
+		//	}));
+		//},
+		//onUploadProgressed: function (oEvent) {
+		//	var oList = this.byId("progressList"),
+		//		oItem = oEvent.getParameter("item");
+		//	oList.insertItem(new ListItem({
+		//		title: "Upload progressed: " + oItem.getFileName()
+		//	}));
+		//},
+		//onUploadCompleted: function (oEvent) {
+		//	var oList = this.byId("progressList"),
+		//		oItem = oEvent.getParameter("item");
+		//	oList.insertItem(new ListItem({
+		//		title: "Upload completed: " + oItem.getFileName()
+		//	}));
+		//},
+		//onUploadAborted: function (oEvent) {
+		//	var oList = this.byId("progressList"),
+		//		oItem = oEvent.getParameter("item");
+		//	oList.insertItem(new ListItem({
+		//		title: "Upload aborted: " + oItem.getFileName()
+		//	}));
+		//},
+		//onFileRenamed: function(oEvent) {
+		//	MessageToast.show("FileRenamed event triggered.");
+		//},
+
+
 
         onAddImage: function() {
             var oUploader = this.getView().byId("imageUploader");
